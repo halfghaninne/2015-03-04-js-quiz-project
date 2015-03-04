ea7444a921e6614bcbf9fc5264a576df2bede9e5
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-03-04 17:16:14 -0600

    created quiz questions

---
+++ quiz.js
@@ -0,0 +1,39 @@
+questionsArray = [{question1:"What is Grumpy Cat's real name?", answer1:"C"}, 
+{question2:"Which one of these is not a famous internet cat?", answer2:"A"}, 
+{question3:"What is the name of the cat on Sabrina the Teenage Witch?", answer3:"D"}, 
+{question4:"What is the name of the cat in Homeward Bound?", answer4:"A"}, 
+{question5:"What cranky internet cat died in 2014?", answer5:"B"}]
+
+
+// What is Grumpy Cat's real name?
+// A. Jeffrey
+// B. Glenda
+// C. Tartar Sauce
+// D. Cocktail Sauce
+
+// Which one of these is not a famous internet cat?
+// A. Michael Jordan
+// B. Nala
+// C. Masha
+// D. Pudge
+
+// What is the name of the cat on Sabrina the Teenage Witch?
+// A. Lucifer
+// B. Satan's Little Helper
+// C. Harry Potter
+// D. Salem
+
+// What is the name of the cat in Homeward Bound?
+// A. Sassy
+// B. Otis
+// C. Stripes
+// D. Socks
+
+// What cranky internet cat died in 2014?
+// A. Grumpy Cat
+// B. Colonel Meow
+// C. Hello Kitty
+// D. Frumpy Cat
+
+
+
